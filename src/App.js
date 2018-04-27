3956b84e2ce4285fa6c06e8960d1d2f5a803d4c0
Russell Cohen <russell.r.cohen@gmail.com>
2018-04-26 17:59:34 -0700

    Add more stations

--- src/App.js
+++ src/App.js
@@ -5,21 +5,48 @@ import "./App.css";
 const flynns = { lat: 37.3884, lon: -118.295, code: "CAZ521", wfo: "VEF" };
 const paiute = { lat: 37.5314, lon: -118.279, code: "CAZ073", wfo: "REV" };
 const mammoth = { lat: 37.63, lon: -119.03, wfo: "REV", code: "CAZ073" };
+const whiteMountain = {
+  lat: 37.6387,
+  lon: -118.2527,
+  wfo: "REV",
+  code: "CAZ073"
+};
+
+const waltsPoint = {
+  lat: 36.4911,
+  lon: -118.1549,
+  wfo: "VEF",
+  code: "CAZ519"
+};
 
 class App extends Component {
   render() {
     const flynnsModel = <NoaaModel loc={flynns} />;
     const paiuteLaunchModel = <NoaaModel loc={paiute} />;
     const mammothModel = <NoaaModel loc={mammoth} />;
+    const mammothLive = (
+      <img src="http://patrol.mammothmountain.com/Reserved.ReportViewerWebControl.axd?ReportSession=tgnfvy45p105qqrv0blap5nz&Culture=1033&CultureOverrides=True&UICulture=1033&UICultureOverrides=True&ReportStack=1&ControlID=15852b0946e8491c8d67d7a4ebcae588&OpType=ReportImage&IterationId=04474ecdfd5a4019a84ddffdff3e2c06&StreamID=C_7iT0_1" />
+    );
     const whiteLive = <img src="https://wrcc.dri.edu/cgi-bin/g2sage.pl?wmtn" />;
     const barLive = <img src="https://wrcc.dri.edu/cgi-bin/g2sage.pl?barc" />;
+    const whiteMountainModel = <NoaaModel loc={whiteMountain} />;
+    const waltsModel = <NoaaModel loc={waltsPoint} />;
+
+    const flynnsLive = <WundergroundImg stationId="KCABISHO50" />;
     return (
       <div className="container">
-        <LabeledImage title="Flynns" src={flynnsModel} />
-        <LabeledImage title="Paiute" src={paiuteLaunchModel} />
-        <LabeledImage title="White Mountain [Paiute]" src={whiteLive} />
-        <LabeledImage title="Mount Barcroft [Paiute]" src={barLive} />
-        <LabeledImage title="Mammoth Mountain" src={mammothModel} />
+        <LabeledImage title="Flynns Forecast" src={flynnsModel} />
+        <LabeledImage title="Flynns Live" src={flynnsLive} />
+        <LabeledImage title="Paiute Launch Forecast" src={paiuteLaunchModel} />
+        <LabeledImage
+          title="White Mountain Forecast"
+          src={whiteMountainModel}
+        />
+        <LabeledImage title="White Mountain Live [sometimes stale]" src={whiteLive} />
+        <LabeledImage title="Mount Barcroft Live [sometimes stale]" src={barLive} />
+        <LabeledImage title="Mammoth Mountain Forecast" src={mammothModel} />
+        <LabeledImage title="Mammoth Mountain Live (in venturi)" src={mammothLive} />
+        <LabeledImage title="Walts Point Forecast" src={waltsModel} />
       </div>
     );
   }
@@ -37,6 +64,21 @@ class LabeledImage extends Component {
         <h3>{this.title}</h3>
         {this.src}
       </div>
+    );
+  }
+}
+
+class WundergroundImg extends Component {
+  constructor({ stationId }) {
+    super();
+    this.imgUrl = `https://www.wunderground.com/cgi-bin/wxStationGraphAll?ID=${stationId}&showwind=1&showwinddir=1&type=3&cacheBuster=1524788397360`;
+    this.imgHref = `https://www.wunderground.com/personal-weather-station/dashboard?ID=${stationId}`;
+  }
+  render() {
+    return (
+      <a href={this.imgHref}>
+        <img src={this.imgUrl} />
+      </a>
     );
   }
 }
